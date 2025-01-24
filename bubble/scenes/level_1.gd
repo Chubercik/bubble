0000003712fc1bb3ad74793ac9cd826eeac2f4ae
Jakub Marcowski <chubercikbattle@gmail.com>
2025-01-24 21:09:20 +0100

    Add basic bubble switching demo

---
+++ bubble/scenes/level_1.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+var bubble_1: CharacterBody2D
+var bubble_2: CharacterBody2D
+var camera: Camera2D
+
+
+func _ready() -> void:
+	bubble_1 = $Bubble_1
+	bubble_2 = $Bubble_2
+	camera = $Camera2D
+
+	bubble_1.active = true
+	camera.reparent(bubble_1, false)
+
+
+func _process(delta: float) -> void:
+	# Quit.
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
+
+	# Switch between bubbles.
+	if Input.is_key_pressed(KEY_1):
+		bubble_1.active = true
+		bubble_2.active = false
+		camera.reparent(bubble_1, false)
+	elif Input.is_key_pressed(KEY_2):
+		bubble_1.active = false
+		bubble_2.active = true
+		camera.reparent(bubble_2, false)
