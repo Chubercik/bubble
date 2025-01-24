00000055ef324dfa3aa7089c201364a647929b5c
Jakub Marcowski <chubercikbattle@gmail.com>
2025-01-25 00:23:39 +0100

    Move all scripts to a dedicated folder

---
+++ bubble/scripts/levels.gd
@@ -0,0 +1,45 @@
+extends Control
+
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_level_1_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+
+func _on_level_2_button_pressed() -> void:
+	pass
+
+
+func _on_level_3_button_pressed() -> void:
+	pass
+
+
+func _on_level_4_button_pressed() -> void:
+	pass
+
+
+func _on_level_5_button_pressed() -> void:
+	pass
+
+
+func _on_level_6_button_pressed() -> void:
+	pass
+
+
+func _on_level_7_button_pressed() -> void:
+	pass
+
+
+func _on_level_8_button_pressed() -> void:
+	pass
+
+
+func _on_level_9_button_pressed() -> void:
+	pass
