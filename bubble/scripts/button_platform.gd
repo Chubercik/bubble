0000020f471f9da388add1e8641a2f5714af7174
Jakub Marcowski <chubercikbattle@gmail.com>
2025-01-28 22:38:41 +0100

    Refactor button logic

--- bubble/scripts/button_platform.gd
+++ bubble/scripts/button_platform.gd
@@ -3,32 +3,49 @@ extends Node2D
 enum Variants {NORMAL, ALT, HEAVY}
 @export var ButtonVariant = Variants.NORMAL
 
-var has_bubble_1 = false
-var has_bubble_2 = false
-var has_bubble_3 = false
-var has_bubble_4 = false
-var has_bubble_5 = false
+var bubbles: Dictionary
 
+var top_sprites: Node2D
 var top: Sprite2D
-var bottom: Sprite2D
 var top_alt: Sprite2D
-var bottom_alt: Sprite2D
 var top_heavy: Sprite2D
+
+var bottom_sprites: Node2D
+var bottom: Sprite2D
+var bottom_alt: Sprite2D
 var bottom_heavy: Sprite2D
+
+var button_hardener: CollisionPolygon2D
 
 var top_pos: Vector2
 var bot_pos: Vector2
 
 
-func _ready() -> void:
-	top = $Area2D/Top
-	top_alt = $Area2D/TopAlt
-	top_heavy = $Area2D/TopHeavy
-	bottom = $StaticBody2D/Bottom
-	bottom_alt = $StaticBody2D/BottomAlt
-	bottom_heavy = $StaticBody2D/BottomHeavy
+func is_active() -> bool:
+	if ButtonVariant == Variants.HEAVY:
+		return bubbles.has("Bubble_3")
+	else:
+		return len(bubbles) > 0
 
-	top_pos = top.position
+
+func _ready() -> void:
+	bubbles = {}
+
+	top_sprites = $Area2D/Sprites
+	top = $Area2D/Sprites/Top
+	top_alt = $Area2D/Sprites/TopAlt
+	top_heavy = $Area2D/Sprites/TopHeavy
+
+	bottom_sprites = $StaticBody2D/Sprites
+	bottom = $StaticBody2D/Sprites/Bottom
+	bottom_alt = $StaticBody2D/Sprites/BottomAlt
+	bottom_heavy = $StaticBody2D/Sprites/BottomHeavy
+
+	button_hardener = $ButtonHardener/CollisionPolygon2D
+
+	button_hardener.set_deferred("disabled", true)
+
+	top_pos = top_sprites.position
 	bot_pos = Vector2(top_pos.x, top_pos.y + 4)
 
 
@@ -57,41 +74,21 @@ func _process(_delta: float) -> void:
 			bottom_heavy.show()
 
 	if ButtonVariant == Variants.HEAVY:
-		if has_bubble_5:
-			top.position = bot_pos
-			top_alt.position = bot_pos
-			top_heavy.position = bot_pos
-	elif has_bubble_1 or has_bubble_2 or has_bubble_3 or has_bubble_4 or has_bubble_5:
-		top.position = bot_pos
-		top_alt.position = bot_pos
-		top_heavy.position = bot_pos
+		if bubbles.has("Bubble_3"):
+			top_sprites.position = bot_pos
+			button_hardener.set_deferred("disabled", true)
+		else:
+			top_sprites.position = top_pos
+			button_hardener.set_deferred("disabled", false)
+	elif len(bubbles) > 0:
+		top_sprites.position = bot_pos
 	else:
-		top.position = top_pos
-		top_alt.position = top_pos
-		bottom_heavy.position = top_pos
+		top_sprites.position = top_pos
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if (body.name == "Bubble_1"):
-		has_bubble_1 = true
-	elif (body.name == "Bubble_2"):
-		has_bubble_2 = true
-	elif (body.name == "Bubble_3"):
-		has_bubble_3 = true
-	elif (body.name == "Bubble_4"):
-		has_bubble_4 = true
-	elif (body.name == "Bubble_5"):
-		has_bubble_5 = true
+	bubbles[body.name] = null
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if (body.name == "Bubble_1"):
-		has_bubble_1 = false
-	elif (body.name == "Bubble_2"):
-		has_bubble_2 = false
-	elif (body.name == "Bubble_3"):
-		has_bubble_3 = false
-	elif (body.name == "Bubble_4"):
-		has_bubble_4 = false
-	elif (body.name == "Bubble_5"):
-		has_bubble_5 = false
+	bubbles.erase(body.name)
