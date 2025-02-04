0000035f49cad273714be467f8a83ecf643de572
Jakub Marcowski <chubercikbattle@gmail.com>
2025-02-04 18:43:30 +0100

    Fix spamming custom mouse cursor

--- bubble/scripts/cursor_handler.gd
+++ bubble/scripts/cursor_handler.gd
@@ -6,12 +6,12 @@ var cursor = load("res://sprites/cursor.png")
 var cursor_click = load("res://sprites/cursor_click.png")
 
 
-func _ready():
+func _ready() -> void:
 	Input.set_custom_mouse_cursor(cursor, Input.CURSOR_ARROW, TIP_LOC)
 
 
 func _process(_delta: float) -> void:
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	if Input.is_action_just_pressed("clicked"):
 		Input.set_custom_mouse_cursor(cursor_click, Input.CURSOR_ARROW, TIP_LOC)
-	else:
+	elif Input.is_action_just_released("clicked"):
 		Input.set_custom_mouse_cursor(cursor, Input.CURSOR_ARROW, TIP_LOC)
