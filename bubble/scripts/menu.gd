00000404a50d529f33aed043a5466cbca815e059
Jakub Marcowski <chubercikbattle@gmail.com>
2025-03-30 13:46:52 +0200

    Turn off the quit game button on web builds

--- bubble/scripts/menu.gd
+++ bubble/scripts/menu.gd
@@ -3,11 +3,13 @@ extends Control
 @onready var volume_bar: Control = $VolumeBar
 @onready var levels: Control = $Levels
 @onready var credits: Control = $Credits
-
+@onready var quit_button: Button = $QuitButton
 
 func _ready() -> void:
 	levels.hide()
 	credits.hide()
+	if OS.has_feature("web"):
+		quit_button.hide()
 
 
 func _on_play_button_pressed() -> void:
